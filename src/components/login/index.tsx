a8249ff5cc48fc555597dfd0af9f7838a90032d1
Tri Nguyen Ngoc <tri.nn@3forcom.com.vn>
2021-01-19 10:28:19 +0700

    add form

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,25 +1,41 @@
 import React from "react";
-import "./index.css"
-import username from "../../image/user-name.svg"
+import { useForm } from "react-hook-form";
+import "./index.css";
 interface LoginProps {
   title: string;
+  username?: string;
+  password?: string;
 }
 const Login: React.FC<LoginProps> = ({ title }) => {
+  const { handleSubmit, register, errors } = useForm<LoginProps>();
+  const onSubmit = (data: LoginProps) => console.log(data);
   return (
-    <form className="login">
+    <form onSubmit={handleSubmit(onSubmit)} className="login">
       <div className="login_title">{title}</div>
       <div className="username">
-          <div className="username_title">UserName</div>
-          <div className="username_input">
-              <img src={username} alt=""/>
-              <input type="text"/>
-          </div>
+        <div className="username_title">UserName</div>
+        <div className="username_input">
+          <input
+            type="text"
+            name="username"
+            ref={register({ required: true })}
+          />
+          <div className="error">{errors.username && <p>Vui lòng nhập tên đăng nhập !</p>}</div>
+        </div>
       </div>
       <div className="login_password">
-          <div className="login_password_title">Password</div>
-          <div className="login_password_input">
-              <input type="password"/>
-          </div>
+        <div className="login_password_title">Password</div>
+        <div className="login_password_input">
+          <input
+            type="text"
+            name="password"
+            ref={register({ required: true })}
+          />
+          <div className="error">{errors.password && <p>Vui lòng nhập mật khẩu !</p>}</div>
+        </div>
+      </div>
+      <div className="submit">
+        <input type="submit" value="Submit" />
       </div>
     </form>
   );
